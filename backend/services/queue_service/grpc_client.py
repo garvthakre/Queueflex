d1004b5e8a6f12badcabee4a48be4462a7255c26
garvthakre <garvthakre0@gmail.com>
2026-04-20 08:54:16 +0530

    feat: implement gRPC client for queue service

--- backend/services/queue_service/grpc_client.py
+++ backend/services/queue_service/grpc_client.py
@@ -1,27 +1,27 @@
 import grpc
+from config.config import GRPC_HOST
+
 import auth_pb2
 import auth_pb2_grpc
 
-CHANNEL = grpc.insecure_channel('localhost:50051')
+CHANNEL = grpc.insecure_channel(GRPC_HOST)
 CLIENT = auth_pb2_grpc.AuthServiceStub(CHANNEL)
+
+
+class FailedResponse:
+    is_valid = False
+    is_admin = False
+    user_id = 0
+
 
 def verify_token(token: str):
     """Verify JWT via auth gRPC"""
     print(f"[QUEUE GRPC] Calling verify...")
-    
     try:
         request = auth_pb2.VerifyTokenRequest(token=token)
         response = CLIENT.VerifyToken(request, timeout=10)
-        
         print(f"[QUEUE GRPC] Response: valid={response.is_valid}, admin={response.is_admin}, user={response.user_id}")
         return response
-        
     except Exception as e:
         print(f"[QUEUE GRPC] Error: {e}")
-        
-        class FailedResponse:
-            is_valid = False
-            is_admin = False
-            user_id = 0
-        
         return FailedResponse()
