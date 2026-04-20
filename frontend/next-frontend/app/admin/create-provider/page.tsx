7e45836bda7bda47cfe94cffc35ac1ca7676b114
garvthakre <garvthakre0@gmail.com>
2026-04-20 08:58:20 +0530

    feat: implement provider creation page

--- frontend/next-frontend/app/admin/create-provider/page.tsx
+++ frontend/next-frontend/app/admin/create-provider/page.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import React, { useState } from "react";
-import authService from "../../services/Authservices";
+import authService from "../../services/authservice";
 import Link from "next/link";
 
 const Page = () => {
