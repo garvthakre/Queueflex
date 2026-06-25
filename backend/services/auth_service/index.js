a54ef93ecd9e6a7d3a5b04d58fe6de59fcf9358b
garvthakre <garvthakre0@gmail.com>
2026-06-25 23:29:22 +0530

    Update auth_service index.js

--- backend/services/auth_service/index.js
+++ backend/services/auth_service/index.js
@@ -1,11 +1,16 @@
 const app = require("./app");
 const { startGrpcServer } = require("./grpc/grpcServer");
+const { initDB } = require("./db/db");
 const config = require("./config/config");
 
-// Start REST API server
-app.listen(config.PORT, () =>
-  console.log(`[AUTH REST] Running on port ${config.PORT}`),
-);
+const start = async () => {
+  await initDB();
 
-// Start gRPC server
-startGrpcServer();
+  app.listen(config.PORT, () =>
+    console.log(`[AUTH REST] Running on port ${config.PORT}`)
+  );
+
+  startGrpcServer();
+};
+
+start();
