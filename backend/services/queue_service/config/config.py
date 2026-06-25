a5285df9185be45c77916a75785c77940a461a12
garvthakre <garvthakre0@gmail.com>
2026-06-25 23:30:08 +0530

    Update queue_service config.py

--- backend/services/queue_service/config/config.py
+++ backend/services/queue_service/config/config.py
@@ -3,6 +3,7 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
+DATABASE_URL = os.environ.get("DATABASE_URL")
 ADMIN_SERVICE_URL = os.environ.get("ADMIN_SERVICE_URL", "http://localhost:5000")
 GRPC_HOST = os.environ.get("AUTH_GRPC_HOST", "localhost:50051")
 PORT = int(os.environ.get("PORT", 4000))
